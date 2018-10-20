ed2f83d4a0595c83170a47b880ac3ab60095b95a
Kojima Naoya <naoyak1989@gmail.com>
2018-10-20 19:37:02 +0900

    Finish 2-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  devise_for :users
+  root "top#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
