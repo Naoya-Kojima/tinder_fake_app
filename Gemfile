fe9e4de7cc020bc1365d8cd249db9b06b5ce0750
Kojima Naoya <naoyak1989@gmail.com>
2018-10-22 13:04:31 +0900

    Finish 3-1

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'devise'
 gem 'bootstrap', '~> 4.1.3'
 gem 'jquery-rails'
 gem "font-awesome-sass"
+gem 'carrierwave', '~> 1.0'
+gem 'rmagick'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  mount_uploader :img_name, ImgNameUploader
 end
